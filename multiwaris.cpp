0259a9abc34fbc33e8b6b018820c730b107a6fed
khansa-qonita <khansaqonita420@gmail.com>
2026-05-19 17:47:44 +0700

    menambahkan class pekerja

--- multiwaris.cpp
+++ multiwaris.cpp
@@ -10,3 +10,9 @@ public :
             cout << "orang dibuat dengan umur" << umur << "\n" << endl;
         }
 };
+
+class pekerja : virtual public orang {
+public :
+    pekerja(int pUmur) :
+        orang(pUmur)
+};
